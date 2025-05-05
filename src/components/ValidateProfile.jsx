af11e40580e66cc606c29c2c7582b3808078fb77
raushan <kraushan022@gmail.com>
2025-05-05 16:00:19 +0530

    validations on phone and password and name min and max length

--- src/components/ValidateProfile.jsx
+++ src/components/ValidateProfile.jsx
@@ -5,6 +5,8 @@ const ValidateProfile = () => {
     firstName: "",
     lastName: "",
     email: "",
+    phone: "",
+    password: "",
   };
 
   const [formData, setFormData] = useState(initialFormData);
@@ -29,16 +31,36 @@ const ValidateProfile = () => {
 
     if (!formData.firstName.trim()) {
       newError.firstName = "FirstName is required";
+    } else if (
+      formData.firstName.length < 2 ||
+      formData.firstName.length > 20
+    ) {
+      newError.firstName = "First name must be 2–20 characters long";
     }
 
     if (!formData.lastName.trim()) {
       newError.lastName = "lastName is required";
+    } else if (formData.lastName.length < 2 || formData.lastName.length > 20) {
+      newError.lastName = "Last name must be 2–20 characters long";
     }
 
     if (!formData.email.trim()) {
       newError.email = "email is required";
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
       newError.email = "Invalid email format";
+    }
+
+    if (!formData.phone.trim()) {
+      newError.phone = "Phone number is required";
+    } else if (!/^\d{10}$/.test(formData.phone)) {
+      newError.phone = "Phone must be exactly 10 digits";
+    }
+
+    if (!formData.password.trim()) {
+      newError.password = "Password must be filled";
+    } else if (formData.password.length < 6 || !/\d/.test(formData.password)) {
+      newError.password =
+        "Password must be at least 6 characters and include a number";
     }
 
     return newError;
@@ -55,8 +77,10 @@ const ValidateProfile = () => {
     }
 
     console.log("email: ", formData.firstName);
-    console.log("password: ", formData.lastName);
-    console.log("password: ", formData.email);
+    console.log("lastName: ", formData.lastName);
+    console.log("email: ", formData.email);
+    console.log("phone: ", formData.phone);
+    console.log("password: ", formData.password);
 
     setFormData(initialFormData);
     setError({});
@@ -108,6 +132,32 @@ const ValidateProfile = () => {
         />
       </label>
       {error.email && <p className="text-red-500 text-sm">{error.email}</p>}
+
+      <label className="block mt-2">
+        Phone:
+        <input
+          type="number"
+          className="border w-full px-2 mt-1"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+      </label>
+      {error.phone && <p className="text-red-500 text-sm">{error.phone}</p>}
+
+      <label className="block mt-2">
+        Password:
+        <input
+          type="text"
+          className="border w-full px-2 mt-1"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      {error.password && (
+        <p className="text-red-500 text-sm">{error.password}</p>
+      )}
 
       <button
         type="submit"
